5094e91735589b75936dec39413a12e45b316c60
Harris Mendell <harrismendell@gmail.com>
2019-03-07 13:44:09 -0500

    respond to comments, clean up

--- functions/src/iot.ts
+++ functions/src/iot.ts
@@ -10,40 +10,28 @@ export const telemetryToFirestore = functions.pubsub.topic('telemetry').onPublis
   const buff = new Buffer( message.data, 'base64' );
   const msg = JSON.parse( buff.toString('utf-8') );
 
-  // TODO: make this generic enough to accept any telemetry data?
-  if (!(msg.hasOwnProperty('light') && msg.hasOwnProperty('temp'))) {
-    throw new Error('Message does not contain fields light or temp');
-  }
-
-  console.log('Received message from', deviceId);
-  console.log('message', msg);
-
   // Write telemetry data to doc's subcollection (will work even if doc doesn't yet exist)
-  const now = Date.now();
-  await db.collection(`devices/${deviceId}/telemetry`).add({
-    time: now,
-    light: msg.light,
-    temp: msg.temp
+  await db.collection(`telemetry`).add({
+    deviceId,
+    ...msg
   });
 
-  // Create device if it doesn't exist
+  // Create device if it doesn't exist``
   const doc = await db.collection('devices').doc(deviceId).get()
   if (!doc.exists) {
-    await db.collection('devices').doc(deviceId).set({
+    await db.collection('devices').doc(deviceId).create({
       uid: null, 
-      createdAt: now
+      createdAt: Date.now()
     });
   }
 })
 
 // TODO: implement stub
-export const stateToFirestore = functions.pubsub.topic('state').onPublish(async (message, context) => {
+export const stateToFirestore = functions.pubsub.topic('settings').onPublish(async (message, context) => {
   const { deviceId } = message.attributes;
-  console.log('message', message, deviceId);
   const buff = new Buffer( message.data, 'base64' );
   const msg = JSON.parse( buff.toString('utf-8') );
-  console.log(msg);
-  await db.doc(`devices/${deviceId}/state`).set(msg);
+  await db.doc(`devices/${deviceId}`).update({ setting: msg.setting });
 })
 
 const dm = new DeviceManager('config-demo');
